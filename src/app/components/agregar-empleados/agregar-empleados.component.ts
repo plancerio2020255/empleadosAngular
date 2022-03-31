572718efaec12d34584350caa20bb8d60bf111d9
plancerio-2020255 <plancerio-2020255@kinal.edu.gt>
2022-03-31 17:33:29 -0600

    Base del proyecto

---
+++ src/app/components/agregar-empleados/agregar-empleados.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agregar-empleados',
+  templateUrl: './agregar-empleados.component.html',
+  styleUrls: ['./agregar-empleados.component.sass']
+})
+export class AgregarEmpleadosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

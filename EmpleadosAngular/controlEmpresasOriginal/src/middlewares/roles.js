78b953f76140d78ed71b0036d02944418c78ecb7
Pedro Alexander Lancerio Alvarado <79710741+plancerio2020255@users.noreply.github.com>
2022-04-06 17:04:20 -0600

    Correcion de roles

--- EmpleadosAngular/controlEmpresasOriginal/src/middlewares/roles.js
+++ EmpleadosAngular/controlEmpresasOriginal/src/middlewares/roles.js
@@ -1,11 +1,11 @@
 exports.verAdmin = function(req, res, next) {
-    if(req.user.rol !== "ROL_SuperAdmin") return res.status(403).send({mensaje: "Solo puede acceder el ADMIN"})
+    if(req.user.rol !== "SuperAdmin") return res.status(403).send({mensaje: "Solo puede acceder el ADMIN"})
     
     next();
 }
 
 exports.verEmpresa = function(req, res, next) {
-    if(req.user.rol !== "ROL_Empresa") return res.status(403).send({mensaje: "Solo puede acceder la EMPRESA"})
+    if(req.user.rol !== "Empresa") return res.status(403).send({mensaje: "Solo puede acceder la EMPRESA"})
     
     next();
 }

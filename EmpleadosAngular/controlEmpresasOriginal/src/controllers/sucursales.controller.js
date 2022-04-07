0dc2aabef53b535889a125bf594867e8a0e6cbd5
Edwar Esaul López Pérez <88805623+elopez2020236@users.noreply.github.com>
2022-04-07 14:03:13 -0600

    Sucursales agregar

---
+++ EmpleadosAngular/controlEmpresasOriginal/src/controllers/sucursales.controller.js
@@ -0,0 +1,33 @@
+const Usuario = require('../models/usuarios.models');
+const Sucursales = require('../models/sucursales.models');
+
+const bcrypt = require('bcrypt-nodejs');
+const jwt = require('../services/jwt');
+
+
+function AgregarSucursal(req, res) {
+    const parametros = req.body;
+    const modeloSucursal = new Sucursales();
+
+    if (parametros.nombreSucursal && parametros.direccion) {
+        modeloSucursal.nombreEmpresa = parametros.nombreEmpresa;
+        modeloSucursal.tipoEmpresa = parametros.tipoEmpresa;
+        modeloSucursal.idEmpresa = req.user.sub;
+
+ 
+
+        modeloSucursal.save((err, sucuralGuardado) => {
+            if (err) return res.status(400).send({ mensaje: 'Error en la peticion' });
+            if (!sucuralGuardado) return res.status(404).send({ mensaje: 'Error al agregar una sucursal' });
+            return res.status(200).send({ Sucursal: sucuralGuardado });
+        })
+
+    } else {
+        return res.status(404).send({ mensaje: 'Debe enviar los parametros obligatorios' })
+    }
+
+}
+
+module.exports = {
+    AgregarSucursal
+}
---
+++ EmpleadosAngular/controlEmpresasOriginal/src/routes/sucursales.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const sucursalControlador = require('../controllers/sucursales.controller');
+const md_autenticacion = require('../middlewares/autenticacion');
+
+const api = express.Router();
+
+
+api.post('/AgregarSucursal', [md_autenticacion.Auth], sucursalControlador.AgregarSucursal);
+
+
+
+module.exports = api;

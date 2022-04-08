32aff5641bcfdbe63fb8fc894008ea8936a331db
Mynor Xocol <mynorxocol123@gmail.com>
2022-04-07 20:09:38 -0600

    sucursales

--- EmpleadosAngular/controlEmpresasOriginal/src/controllers/sucursales.controller.js
+++ EmpleadosAngular/controlEmpresasOriginal/src/controllers/sucursales.controller.js
@@ -1,3 +1,4 @@
+const Usuario = require('../models/usuarios.models');
 const Sucursales = require('../models/sucursales.models');
 const bcrypt = require('bcrypt-nodejs');
 const jwt = require('../services/jwt');

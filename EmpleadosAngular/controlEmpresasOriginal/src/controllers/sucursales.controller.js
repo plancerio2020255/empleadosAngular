1abf46e34e8133ad27eb25f3d5f4fe2c3cbc82da
Mynor Xocol <mynorxocol123@gmail.com>
2022-04-07 20:01:30 -0600

    commit

--- EmpleadosAngular/controlEmpresasOriginal/src/controllers/sucursales.controller.js
+++ EmpleadosAngular/controlEmpresasOriginal/src/controllers/sucursales.controller.js
@@ -2,3 +2,22 @@ const Usuario = require('../models/usuarios.models');
 const Sucursales = require('../models/sucursales.models');
 const bcrypt = require('bcrypt-nodejs');
 const jwt = require('../services/jwt');
+
+function EditarSucursales(req, res) {
+    const parametros = req.body;
+    const idEmpleado = req.params.idEmpleado;
+
+    Sucursales.findOne({ _id: idsucursales, idEmpresa: req.user.sub }, (err, SucursalEncontrada) => {
+        if (!empresaEncontrada) {
+            return res.status(400).send({ mensaje: "No puedes editar" });
+        }
+        Sucursales.findByIdAndUpdate(idEmpresa, parametros, { new: true },
+            (err, sucursalActualizado) => {
+                if (err) return res.status(500).send({ mensaje: 'Error en la peticion' });
+                if (!sucursalActualizado) return res.status(500).send({ mensaje: 'Error al Editar Sucursal' });
+
+                return res.status(200).send({ sucursal: sucursalActualizado })
+            }
+        );
+    })
+}

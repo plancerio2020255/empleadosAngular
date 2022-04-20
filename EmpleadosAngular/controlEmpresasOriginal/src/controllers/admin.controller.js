36fa4629e33cd4cbf0ef9b7ee46a0f96be5a885a
Pedro Alexander Lancerio Alvarado <79710741+plancerio2020255@users.noreply.github.com>
2022-04-20 14:26:38 -0600

    Edicion de modelos y creacion de funciones

---
+++ EmpleadosAngular/controlEmpresasOriginal/src/controllers/admin.controller.js
@@ -0,0 +1,104 @@
+const Empresas = require('../models/empresa.model');
+
+const Tipo = require('../models/tipoEmpresa.model');
+
+const Municipios = require('../models/municipios.model');
+
+const bcrypt = require('bcrypt-nodejs');
+
+
+
+function crearAdmin(req, res) {
+    const administrador = new Empresas();
+
+    Empresas.findOne({ usuario: 'SuperAdmin' }, (err, crearAdmin) => {
+        if (err) {
+            console.log('Error al crear usuario administrador');
+        } else if (crearAdmin) {
+            console.log('Ya existe un usuario administrador');
+        } else {
+            bcrypt.hash('123456', null, null, (err, contraseñaEncriptada) => {
+                if (err) {
+                    res.status(500).send({ mensaje: 'Error al encriptar contraseña' })
+                } else if (contraseñaEncriptada) {
+                    administrador.nombre = 'SuperAdmin'
+                    administrador.usuario = 'SuperAdmin'
+                    administrador.email = 'SuperAdmin'
+                    administrador.password = contraseñaEncriptada
+                    administrador.rol = 'SuperAdmin'
+                    administrador.save((err, usuarioGuardado) => {
+                        if (err) {
+                            console.log('Error al crear usuario administrador');
+                        } else if (usuarioGuardado) {
+                            console.log('Usuario administrador creado con extio, bienvenido: ' + usuarioGuardado.nombre);
+                        } else {
+                            console.log('Usuario administrador no creado');
+                        }
+                    })
+                }
+            })
+        }
+    })
+
+}
+
+//------------------- Municipios------------------ //
+
+function crearMunicipio() {
+
+}
+
+
+function editarMunicipio() {
+
+}
+
+function deleteMunicipio() {
+
+}
+
+// ---------------- Tipo Empresas ----------------
+
+function crearTipoEmpresa() {
+
+}
+
+function editarTipoEmpresa() {
+
+}
+
+function deleteTipoEmpresa() {
+
+}
+
+//--------------------- Empresas -------------------------------- //
+
+function agregarEmpresa() {
+
+}
+
+function editarEmpresa() {
+
+}
+
+function eliminarEmpresa() {
+
+}
+
+
+
+module.exports = {
+    crearAdmin,
+    //-------- Municipios ------//
+    crearMunicipio,
+    editarMunicipio,
+    deleteMunicipio,
+    // -------- Empresas ------//
+    agregarEmpresa,
+    editarEmpresa,
+    eliminarEmpresa,
+    // -------- Tipo Empresas ------//
+    crearTipoEmpresa,
+    editarTipoEmpresa,
+    deleteTipoEmpresa,
+}
--- EmpleadosAngular/controlEmpresasOriginal/src/models/empresa.model.js
+++ EmpleadosAngular/controlEmpresasOriginal/src/models/empresa.model.js
@@ -1,12 +1,13 @@
-const mongoose = require('moongose');
+const mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
 var EmpresaSchema = Schema({
     nombre: String,
-    rol: String,
-    idTipoEmpresa: {type: Schema.Types.ObjectId, ref:'Tipo'},
-    idProductos: {type: Schema.Types.objectId, ref:'Productos'},
+    usuario: String,
+    email: String,
+    password: String,
+    rol: String
 })
 
-module.exports = mongoose.model('Empresas', EmpresaSchema)
+module.exports = mongoose.model('Empresas', EmpresaSchema);

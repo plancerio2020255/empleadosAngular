a5f9962ff7c7d8cd9bc7a7cecf125200101d063f
Pedro Alexander Lancerio Alvarado <79710741+plancerio2020255@users.noreply.github.com>
2022-04-26 16:43:00 -0600

    Update ver-empresa.component.ts

--- EmpleadosAngular/empleadosAngular/src/app/components/ver-empresa/ver-empresa.component.ts
+++ EmpleadosAngular/empleadosAngular/src/app/components/ver-empresa/ver-empresa.component.ts
@@ -3,13 +3,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ver-empresa',
   templateUrl: './ver-empresa.component.html',
-  styleUrls: ['./ver-empresa.component.sass']
+  styleUrls: ['./ver-empresa.component.sass'],
+  providers: [EmpresasService]
 })
 export class VerEmpresaComponent implements OnInit {
+  
+  public empresaModelGet: Empresas;
+  public token;
 
-  constructor() { }
+  constructor(private empresaService: EmpresasService) { 
+     this.token = this.empresaService.getToken();
+  }
 
   ngOnInit(): void {
+     this.getEmpresas();
+  }
+  
+    getEmpresas(){
+    this.empresaService.obtenerEmpresas(this.token).subscribe(
+      (res) => {
+        this.empresaModelGet = res.Empresas;
+        console.log(res.Empresas);
+      },
+      (error) => {
+        console.log(<any>error);
+      }
+    )
   }
 
 }

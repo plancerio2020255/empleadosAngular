e2b46734aaf20c76854031d1d40f92be3551cbdb
Pedro Alexander Lancerio Alvarado <79710741+plancerio2020255@users.noreply.github.com>
2022-04-26 17:54:54 -0600

    Update app.js

--- EmpleadosAngular/controlEmpresasOriginal/app.js
+++ EmpleadosAngular/controlEmpresasOriginal/app.js
@@ -1,14 +1,16 @@
-const mongoose = require('mongoose')
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-const Schema = mongoose.Schema
+const empresasRoute = require('./src/routes/empresa.routes')
+const adminRoute = require('./src/routes/admin.routes')
 
-const EmpresaSchema = Schema({
-  nombre: String,
-  usuario: String,
-  email: String,
-  password: String,
-  rol: String,
-  tipoEmpresa: String
-})
 
-module.exports = mongoose.model('Empresas', EmpresaSchema)
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
+app.use(cors());
+
+app.use('/api', empresasRoute, adminRoute) 
+
+module.exports = app;

09a1e9a1f62d49de9f81c3e9c6258ba0d89b49f1
Pedro Alexander Lancerio Alvarado <79710741+plancerio2020255@users.noreply.github.com>
2022-04-26 18:00:03 -0600

    Update admin.routes.js

--- EmpleadosAngular/controlEmpresasOriginal/src/routes/admin.routes.js
+++ EmpleadosAngular/controlEmpresasOriginal/src/routes/admin.routes.js
@@ -4,8 +4,14 @@ const md_autenticacion = require('../middlewares/autenticacion');
 const md_role = require('../middlewares/roles');
 const api = express.Router()
 
+// Empresas
 api.post('/agregarEmpresa', [md_autenticacion.Auth, md_role.verAdmin], controladorAdmin.agregarEmpresa)
-
+api.put('/editarEmpresa', [md_autenticacion.Auth, md_role.verAdmin], controladorAdmin.editarEmpresa)
+api.delete('/eliminarEmpresa', [md_autenticacion.Auth, md_role.verAdmin], controladorAdmin.eliminarEmpresa)
+api.get('/verEmpresa', [md_autenticacion.Auth, md_role.verAdmin], controladorAdmin.verEmpresa)
+//Municipios
 api.post('/crearMunicipio', [md_autenticacion.Auth, md_role.verAdmin], controladorAdmin.crearMunicipio)
+api.put('/editarMunicipio', [md_autenticacion.Auth, md_role.verAdmin], controladorAdmin.editarMunicipio)
+api.put('/eliminarMunicipio', [md_autenticacion.Auth, md_role.verAdmin], controladorAdmin.deleteMunicipio)
 
 module.exports = api

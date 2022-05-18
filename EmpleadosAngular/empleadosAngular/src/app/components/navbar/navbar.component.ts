941a20c94b9790dcb1a66b02d785a38b5afa1076
JFranco-2020338 <101079444+JFranco-2020338@users.noreply.github.com>
2022-05-17 19:10:53 -0600

    Eliminacion serice

--- EmpleadosAngular/empleadosAngular/src/app/components/navbar/navbar.component.ts
+++ EmpleadosAngular/empleadosAngular/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { EmpresasService } from 'src/app/services/empresa.service';
+
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.sass'],
-  providers: [EmpresasService]
+
 })
 export class NavbarComponent implements OnInit {
   public identidad;

4cb1baed39fe786b371f14214a100047a639ae91
JFranco-2020338 <101079444+JFranco-2020338@users.noreply.github.com>
2022-05-22 23:44:25 -0600

    jeff

--- EmpleadosAngular/empleadosAngular/src/app/components/login/login.component.ts
+++ EmpleadosAngular/empleadosAngular/src/app/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Usuario } from 'src/app/models/usuario.model';
 import { UsuarioService } from 'src/app/services/usuario.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -63,16 +64,28 @@ export class LoginComponent implements OnInit {
         this.getTokenPromesa().then(respuesta=>{
         console.log(response.usuario);
         localStorage.setItem('identidad', JSON.stringify(response.usuario))
-        this._router.navigate(['/'])
-
-        });
+        this._router.navigate([''])
+        })
+        Swal.fire({
+          icon: 'success',
+          title: 'Logeado correctamente',
+          showConfirmButton: false,
+          timer: 1500
+        })
       },
       (error)=>{
         console.log(<any>error);
+        Swal.fire({
+          icon: 'error',
+          title: 'No se pudo loguiar',
+          showConfirmButton: false,
+          timer: 1500
+        })
       }
     );
   }
 
+  
 
 
 }
